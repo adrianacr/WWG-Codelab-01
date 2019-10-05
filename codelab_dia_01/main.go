77155073fb52d2b342cc391591dd3aa69c86c2f1
Adriana <acorrearodrigues@gmail.com>
2019-10-05 16:45:59 -0300

    Ex 01 resolvido

--- codelab_dia_01/main.go
+++ codelab_dia_01/main.go
@@ -4,8 +4,15 @@ import "fmt"
 
 func main() {
 	println("exercicio 01 do lab 1 :D")
-	var numero int
-	numero = 5
-	fmt.Println('\n', numero)
+	var primeiroNumero int
+	var segundoNumero int
+	var terceiroNumero int
+	primeiroNumero = 5
+	segundoNumero = 10
+	terceiroNumero = 15
+
+	fmt.Println("Primeiro numero", primeiroNumero)
+	fmt.Println("Segundo numero", segundoNumero)
+	fmt.Println("Terceiro numero", terceiroNumero)
 
 }
